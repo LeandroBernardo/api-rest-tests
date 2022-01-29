5cb1698e650850c77a2942070e645d53c5f00c88
Leandro Bernardo <lbsantos1@stefanini.com>
2022-01-29 12:19:22 -0300

    class user resource findById

--- src/main/java/br/com/projectwithtests/api/resource/UserResource.java
+++ src/main/java/br/com/projectwithtests/api/resource/UserResource.java
@@ -1,6 +1,8 @@
 package br.com.projectwithtests.api.resource;
 
 import br.com.projectwithtests.api.domain.User;
+import br.com.projectwithtests.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,10 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findbyId(@PathVariable  Integer id){
 
-        return ResponseEntity.ok().body(new User(1, "Leandro", "leandro.bernardos@gmail.com", "123"));
+        return ResponseEntity.ok().body(service.findById(id));
 
     }
 }
