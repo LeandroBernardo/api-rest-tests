7f3253fdebf6af56345f241c3907147b1f58e1fd
Leandro Bernardo <lbsantos1@stefanini.com>
2022-01-29 12:19:05 -0300

    interface user impl findById

---
+++ src/main/java/br/com/projectwithtests/api/services/impl/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package br.com.projectwithtests.api.services.impl;
+
+import br.com.projectwithtests.api.domain.User;
+import br.com.projectwithtests.api.repositories.UserRepository;
+import br.com.projectwithtests.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public User findById(Integer id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.orElse(null);
+    }
+}
